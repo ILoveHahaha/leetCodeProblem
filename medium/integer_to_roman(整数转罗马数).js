98f7a55191f08dae263dc2b5bfbec301567d1f29
ILoveHahaha <zhengbanbugu@163.com>
2019-05-15 01:07:25 +0800

    修改了第十二题

--- medium/integer_to_roman(整数转罗马数).js
+++ medium/integer_to_roman(整数转罗马数).js
@@ -1,3 +1,7 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
 let intToRoman = function(num) {
     let tempObj = {
         1: 'I', 4: 'IV', 5: 'V', 9: 'IX',
@@ -16,3 +20,13 @@ let intToRoman = function(num) {
     }
     return res
 };
+
+
+let intToRoman1 = function (num) {
+    var temp1 = ["", "M", "MM", "MMM"];
+    var temp2 = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    var temp3 = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    var temp4 = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+
+    return temp1[Math.floor(num / 1000)] + temp2[Math.floor(num % 1000 / 100)] + temp3[Math.floor(num % 100 / 10)] + temp4[num % 10];
+}
