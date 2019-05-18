d8a44a3a11177b7dc74909f8621a11f3f4b69163
ILoveHahaha <zhengbanbugu@163.com>
2019-05-18 20:57:08 +0800

    修改了第十四题

--- easy/longest_common_prefix(最长公共前缀).js
+++ easy/longest_common_prefix(最长公共前缀).js
@@ -30,3 +30,35 @@ var longestCommonPrefix = function(strs) {
     }
     return temp
 };
+
+var longestCommonPrefix1 = (strs) => {
+    if (!strs) return '';
+
+    // 数组长度小于2，直接返回空
+    const length = strs.length;
+    if (length < 2) return strs[0] || '';
+
+    // 有空元素直接返回空
+    if (/\b(undefined|null|\s)\b/.test(strs.join())) return '';
+
+    let firstStr = strs[0];
+
+    // 递归进行二分法查找
+    const loop = (start, end, str) => {
+        // 下标超出范围, 结束并返回空字符串
+        if (start > end) return '';
+
+        // 取中间下标, >> 右移一位相当于除2, 右移n位相当于除以2的n次方。
+        const middle = start + ((end - start) >> 1);
+
+        // 截取字符，循环遍历是否存在于所有元素，根据情况进行下一轮二分查找
+        let s = str.slice(0, middle);
+        for (let i = 1; i < length; i += 1) {
+            if (strs[i].slice(0, middle) !== s) return loop(start, middle - 1, s) || '';
+        }
+        return loop(middle + 1, end, str) || s;
+    };
+
+    // loop(startIndex + 1， endIndex + 1, str)
+    return loop(1, firstStr.length, firstStr);
+};
